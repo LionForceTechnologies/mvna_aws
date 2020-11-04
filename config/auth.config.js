a3aed277a8dc052dd8abae2c160272db5c3dca8b
giri <giri@lionforce.net>
2020-11-04 10:21:05 +0530

    first commit

---
+++ config/auth.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    secret: process.env.secret ? process.env.secret  : "development-env-secret",
+    expiryTime: "60m",
+    secretNew:'secret',
+    apikey : 'BLwMDmvsPq8JFIkdp0uOa9x75',
+    apiSecretKey : 'pRm4XFWGfnOjE1rQdNnMRGb94nwKnD2no6hMXQTOCBFrXzSWmn',
+    accessToken : '1303194669312999425-RvvkkMWFQ4BRth6BdNjP7MuYuEDTvg',
+    accessTokenSecret : 'IRrC0iqYXPKbRlapBtH3EOQJF0xyjDWYWp1jQtyJVsiSW',
+    bearer_token : 'AAAAAAAAAAAAAAAAAAAAAMpCHgEAAAAAPKgNoHjdSepObnC4w%2FtQK2VYLJM%3DXEUXtltScyMi0G31ANRxYm5p13MhztkKxhzpoNdhPiI81L0D7W',
+    screen_name:'giriram10307593',
+    count:100,
+  };
+  
